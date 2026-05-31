248ee3e8ca911ac10ff7c398aabad7c9f2e77dba
prince kumar <princekumar22205@gmail.com>
2026-05-31 16:14:06 +0530

    shift the dns code to server.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,10 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
     try{
         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("database is connected");
-        console.log("readyState =", mongoose.connection.readyState);
     }
     catch(err){
         console.log("MongoDB Connection Error:", err.message);
